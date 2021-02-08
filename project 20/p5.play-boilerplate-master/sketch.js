a6fcb4a3155fb2cc2bb3efbffec086e909834a09
yogitaGarg12345 <75129225+yogitaGarg12345@users.noreply.github.com>
2021-02-08 04:16:25 -0800

    Add files via upload

---
+++ project 20/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,57 @@
+
+function setup() {
+  createCanvas(1000, 2200);
+
+
+	
+}
+
+
+function draw() {
+  background(150);
+
+ 
+  
+
+
+
+
+
+textSize(15);
+  fill("blue");
+  text("1 teaspoon pure vanilla extract",100,380)
+text("3 cups (12 ounces) walnut halves",100,360)
+text(" 2 cups quartered moist dates, or any other favorite dried fruits",100,420);
+text("1 cup dried apricots, plums, pluots, pears, or peaches, or a mixture (to measure, leave apricots or similar-size fruit whole and cut ",100,320);
+text("2 and 1/2 cup sugar",100,300);
+text("larger fruit in halves or thirds)",100,340)
+text("1/2 teaspoon salt",100,280);
+text("1/4 teaspoon baking powder",100,240);
+text("1/4 teaspoon baking soda",100,220)
+text("3/4 cup (3.4 ounces) all-purpose flour ",100,200)
+text("2 cups curd",100,400);
+text("1 cup oil",100,260);
+
+
+
+
+
+
+textSize(15);
+fill("blue");
+text("Position a rack in the lower third of the oven and preheat the oven to 300°F (150°C). Spray the loaf pans (either one 9-by-5-inch ",100,550)
+text("8-cup loaf pan or two 8-by-4-inch 4-cup loaf pans) with vegetable oil spray or line the bottom and sides with parchment paper.",100,570)
+text("In a large bowl, whisk the flour with the baking soda, baking powder, and salt to combine. Add  sugar, all the dried ",100,610)
+text("fruit, and the nuts and mix thoroughly with your fingers. Set aside.",100,630)
+
+textSize(20)
+fill("black")
+text("Directions",100,500)
+text("Ingredients",100,150)
+
+textSize(30);
+fill("red");
+ text("Fruits and Nuts cake",400,50);
+  drawSprites();
+ 
+}
